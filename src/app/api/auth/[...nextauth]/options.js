4923440901d3b75e41a1eeffbeb66e4ef354965b
Alessandro Rodrigues <alessandro.rodrigues@cieers.org.br>
2025-04-28 11:32:20 -0300

    altera página de login para logar por credentials ou github

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -70,4 +70,7 @@ export const options = {
       return session;
     },
   },
+  pages: {
+    signIn: "/signin",
+  },
 };
--- src/components/ProviderLogin/index.jsx
+++ src/components/ProviderLogin/index.jsx
@@ -1,11 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import { IconButton } from "../IconButton";
 
 import githubImg from "./github.png";
+import { signIn } from "next-auth/react";
 
 export const Github = (props) => {
+  function loginAttempt() {
+    console.log("Tenta login via github");
+    signIn("github", {
+      callbackUrl: "/",
+    });
+  }
   return (
-    <IconButton {...props}>
+    <IconButton {...props} onClick={loginAttempt}>
       <Image src={githubImg} alt="Github Logo" />
     </IconButton>
   );
