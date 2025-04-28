e2863cc12f3d037ccdf4f1bb528673c7096fee8e
Alessandro Rodrigues <alessandro.rodrigues@cieers.org.br>
2025-04-28 14:02:30 -0300

    habilita comentários quando logado

--- src/components/IconButton/index.jsx
+++ src/components/IconButton/index.jsx
@@ -1,7 +1,9 @@
-import styles from './iconbutton.module.css'
+import styles from "./iconbutton.module.css";
 
-export const IconButton = ({ children, ...rest }) => {
-    return (<button {...rest} className={styles.btn}>
-        {children}
-    </button>)
-}
+export const IconButton = ({ children, disabled, ...rest }) => {
+  return (
+    <button {...rest} className={styles.btn} disabled={disabled}>
+      {children}
+    </button>
+  );
+};
--- src/components/ModalComment/index.jsx
+++ src/components/ModalComment/index.jsx
@@ -1,35 +1,42 @@
-'use client'
+"use client";
 
-import { useRef } from "react"
-import { IconButton } from "../IconButton"
-import { Modal } from "../Modal"
-import { Chat } from "../icons/Chat"
-import { Textarea } from "../Textarea"
+import { useRef } from "react";
+import { IconButton } from "../IconButton";
+import { Modal } from "../Modal";
+import { Chat } from "../icons/Chat";
+import { Textarea } from "../Textarea";
+import { useSession } from "next-auth/react";
 
-import styles from './commentmodal.module.css'
-import { SubmitButton } from "../SubmitButton"
-import { Subheading } from "../Subheading"
+import styles from "./commentmodal.module.css";
+import { SubmitButton } from "../SubmitButton";
+import { Subheading } from "../Subheading";
 
 export const ModalComment = ({ action }) => {
-    const modalRef = useRef(null)
-    return (
-        <>
-            <Modal ref={modalRef}>
-                <form action={action} onSubmit={() => modalRef.current.closeModal()}>
-                    <Subheading>Deixe seu comentário sobre o post:</Subheading>
-                    <Textarea required rows={8} name="text" placeholder="Digite aqui..." />
-                    <div className={styles.footer}>
-                        <SubmitButton>
-                            Comentar
-                        </SubmitButton>
-                    </div>
-                </form>
-            </Modal>
-            <IconButton
-                onClick={() => modalRef.current.openModal()}
-            >
-                <Chat />
-            </IconButton>
-        </>
-    )
-}
+  const modalRef = useRef(null);
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <Modal ref={modalRef}>
+        <form action={action} onSubmit={() => modalRef.current.closeModal()}>
+          <Subheading>Deixe seu comentário sobre o post:</Subheading>
+          <Textarea
+            required
+            rows={8}
+            name="text"
+            placeholder="Digite aqui..."
+          />
+          <div className={styles.footer}>
+            <SubmitButton>Comentar</SubmitButton>
+          </div>
+        </form>
+      </Modal>
+      <IconButton
+        disabled={!session}
+        onClick={() => modalRef.current.openModal()}
+      >
+        <Chat />
+      </IconButton>
+    </>
+  );
+};
